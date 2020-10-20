d3350d5910b75e5bf9774297fd14aab7b73f98c3
ahijevyc <ahijevyc@ucar.edu>
2020-10-20 13:56:59 -0600

    [0,1] range for BAGSS and GSS, not [0,0.8]

--- statisticplot.py
+++ statisticplot.py
@@ -3,8 +3,8 @@ def limits(stat):
         'BRIER'   : [ 0   ,0.15],
         'CSI'     : [ 0   ,1],
         'FSS'     : [ 0   ,1],
-        'GSS'     : [ 0   ,0.8],
-        'BAGSS'   : [ 0   ,0.8], # bias adjusted GSS
+        'GSS'     : [ 0   ,1],
+        'BAGSS'   : [ 0   ,1], # bias adjusted GSS
         'HK'      : [-0.2 ,1],
         'HSS'     : [-0.2 ,1],
         'BSS_SMPL': [-1   ,1],
