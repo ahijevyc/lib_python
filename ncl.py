381b60f5e2cae1fb5d37b01be962147791c338a8
ahijevyc <ahijevyc@ucar.edu>
2020-12-22 10:38:50 -0700

    less verbose unless debug is set

--- ncl.py
+++ ncl.py
@@ -1,16 +1,19 @@
 import subprocess
-import pdb
 import pandas as pd
 
 
-def steering_flow(args = dict(stormname="Joaquin", storm_heading=45, storm_speed=3.2, lat0=24.3, lon0=-74.3, force_new=False, fhr=000, rx=4.5, pbot=850, ptop=200, file_ncl="/glade/scratch/ahijevyc/NARR/merged_AWIP32.2015100300.3D", ensmember="JOAQUIN2015", nc_output=False), debug=False, **kwargs):
+def steering_flow(args = dict(stormname="Joaquin", storm_heading=45, storm_speed=3.2, lat0=24.3, lon0=-74.3, 
+    force_new=False, fhr=000, rx=4.5, pbot=850, ptop=200, 
+    file_ncl="/glade/scratch/ahijevyc/NARR/merged_AWIP32.2015100300.3D", ensmember="JOAQUIN2015", nc_output=False), debug=False, **kwargs):
     # return DataFrame with results of steering_flow.ncl
     # For example, wind_shear_heading, steering_flow_speed, etc. 
 
     # Replace default keyword arguments with provided ones (in kwargs dictionary). 
     for k in kwargs:
         args[k] = kwargs[k]
-    arglist = ["ncl"]
+    arglist = ["ncl", "-Q"] # -Q Turn off echo of NCL version and copyright info
+    if debug:
+        arglist.remove("-Q")
 
     # Put args into strings suitable for ncl command line.
     for k in args:
@@ -23,11 +26,13 @@ def steering_flow(args = dict(stormname="Joaquin", storm_heading=45, storm_speed
 
     nclscript = "/glade/work/ahijevyc/ncl/steering.ncl"
     arglist.append(nclscript)
-    print("ncl.steering_flow():", arglist)
+    if debug:
+        print("ncl.steering_flow():", arglist)
     ncls = subprocess.run(arglist, stdout=subprocess.PIPE, encoding='utf-8')
     if debug:
         print("ncl.steering_flow: exit code was %d" % ncls.returncode)
-    print("ncl.steering_flow: stdout "+ncls.stdout)
+    if debug or len(ncls.stdout.split("\n")) > 10: # force print if there's more standard output than usual
+        print("ncl.steering_flow() stdout ", ncls.stdout)
     # Parse standard output for csv output filename.
     for line in ncls.stdout.split("\n"):
         if 'csv_output_file:' in line:
