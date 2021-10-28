638f753d32bc919fd7a12992f3e96518d2d789b5
ahijevyc <ahijevyc@ucar.edu>
2021-10-28 10:33:33 -0600

    Ensure correct units for unit-unaware NCL script.

--- ncl.py
+++ ncl.py
@@ -1,11 +1,16 @@
-import subprocess
+from metpy.units import units
 import pandas as pd
+import subprocess
 
 
-def steering_flow(args = dict(stormname="Joaquin", storm_heading=45, storm_speed=3.2, lat0=24.3, lon0=-74.3, 
-    force_new=False, fhr=000, rx=4.5, pbot=850, ptop=200, 
+def steering_flow(args = dict(stormname="Joaquin", storm_heading=45*units("degrees"), storm_speed=3.2*units("m/s"), 
+    lat0=24.3*units("degrees_north"), lon0=-74.3*units("degrees_east"), 
+    force_new=False, fhr=000, rx=4.5*units("degrees"), pbot=850*units("hPa"), ptop=200*units("hPa"), 
     file_ncl="/glade/scratch/ahijevyc/NARR/merged_AWIP32.2015100300.3D", ensmember="JOAQUIN2015", nc_output=False), debug=False, **kwargs):
-    # return DataFrame with results of steering_flow.ncl
+
+    # Ensure correct units for unit-unaware NCL script
+    kwargs["storm_speed"] = kwargs["storm_speed"].to("m/s").m
+    # return Pandas Series with results of steering_flow.ncl
     # For example, wind_shear_heading, steering_flow_speed, etc. 
 
     # Replace default keyword arguments with provided ones (in kwargs dictionary). 
@@ -38,7 +43,9 @@ def steering_flow(args = dict(stormname="Joaquin", storm_heading=45, storm_speed
         if 'csv_output_file:' in line:
             words = line.split()
             csv_file = words[-1]
+            #Tried squeeze=True to return a Series, but still DataFrame
             df = pd.read_csv(csv_file)
+            df = df.squeeze()
             # Once we've found and read csv file, stop parsing standard output.
             break
     return df 
